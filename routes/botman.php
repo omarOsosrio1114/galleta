48a9fbd22358f591343a92d7651b08400c506bb8
omar osorio <omarelmejor@outlook.es>
2018-10-27 12:14:17 -0500

    funcionalidad de romper galleta

--- routes/botman.php
+++ routes/botman.php
@@ -1,9 +1,12 @@
 <?php
 use App\Http\Controllers\BotManController;
+use App\Cookie;
 
 $botman = resolve('botman');
 
-$botman->hears('Hi', function ($bot) {
-    $bot->reply('Hello!');
+$botman->hears('romper galleta', function ($bot) {
+    $cookie  = Cookie::all()->random(1)->first();
+
+    $bot->reply($cookie->message);
 });
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
